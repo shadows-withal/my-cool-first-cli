9ae7a0c407afbf98746dbbe62db5009a7ff92bfa
liv <shadows_withal@fastmail.com>
2023-10-09 13:16:45 +0200

    step 2, enhancing clap

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use clap::Parser;
 
 /// A simple program that prints a message, but it's cute!
 #[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
 struct Arguments {
+    /// The message to print.
     message: String,
 }
 
